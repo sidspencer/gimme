6d15f6400dc7946e2e90dc44c7f838765688af47
Sid Spencer <dis@bub.local>
2017-01-15 10:09:54 -0800

    Change how it interacts with popup.js

--- background/Output.js
+++ background/Output.js
@@ -4,8 +4,12 @@
  * Factory function for creating an output stream.
  */
 var Output = (function Output(dokken) {
-    var me = {};
+    var me = {
+        ACTION_HOLDER_ID: 'actionHolder',
+        FILES_DUG_ID: 'filesDug',
+    };
     me.doc = dokken;
+
     me.filesDug = me.doc.getElementById('filesDug');
     me.out = me.doc.getElementById('output');
 
@@ -61,12 +65,59 @@ var Output = (function Output(dokken) {
     me.addNewEntry = function addNewEntry(id, entry) {
         var newLi = me.doc.createElement('li');
         
-        newLi.innerHTML = entry;
+        var newContent = document.createTextNode(entry);
         newLi.id = 'fileEntry' + id;
+        newLi.className = 'found';
+        newLi.appendChild(newContent);
+        
+        me.filesDug.appendChild(newLi);
+    };
+
+
+    /**
+     * Add a checkbox-label pair to the list. The checkbox is clickable to download the uri listed
+     * in the label. 
+     */
+    me.addFileOption = function addFileOption(fileOpt) {
+        var checkbox = me.doc.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.name = 'cbFile' + fileOpt.id;
+        checkbox.id = checkbox.name;
+        checkbox.value = fileOpt.uri;
+        checkbox.dataset.filePath = fileOpt.filePath;       
+
+        var nameLabel = me.doc.createElement('label');
+        nameLabel.setAttribute('for', checkbox.name);
+        var nameContent = document.createTextNode(fileOpt.filePath);
+        nameLabel.appendChild(nameContent);
+
+        var newLi = me.doc.createElement('li');
+        newLi.appendChild(checkbox);
+        newLi.appendChild(nameLabel);
+        newLi.id = 'fileEntry' + fileOpt.id;
         newLi.className = 'found';
         
         me.filesDug.appendChild(newLi);
-    }
+
+        me.doc.getElementById(checkbox.id).addEventListener('click', function whenFileOptClicked(event) {
+            var cb = event.currentTarget;
+            console.log('WhenClicked! ' + event.currentTarget.id + '');
+
+            if (!!cb.dataset.filePath) {
+                event.currentTarget.disabled = true;
+                fileOpt.onSelect(cb.value, cb.dataset.filePath);
+                cb.dataset.filePath = '';
+            }
+        });
+    };
+
+
+    /**
+     * Should we show the "action" buttons for the file option list?
+     */
+    me.showActionButtons = function showActionButtons() {
+        me.doc.getElementById(me.ACTION_HOLDER_ID).style.display = 'block';
+    };
 
 
 
