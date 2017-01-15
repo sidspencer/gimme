7dba6ab030aba719f1b5f946cbe76a9e64688a26
Sid Spencer <dis@bub.local>
2017-01-15 10:05:53 -0800

    Remove cruft, slightly change styling, add new actions.

--- popup/popup.js
+++ popup/popup.js
@@ -4,7 +4,38 @@
  * Set up event handlers for the UI. All actual work is done in the background scripts.
  */
 document.addEventListener("DOMContentLoaded", function init() {
-    
+    /**
+     * Scrape and dig, but don't automatically download. Instead, present the user with checkbox options
+     * as to what files to download.
+     */
+    document.getElementById('digFileOptionsButton').addEventListener('click', function onDigFileOptions() {
+        chrome.runtime.getBackgroundPage(function doDiggingForOptions(bgWindow) {
+            bgWindow.goDigFileOptions(window.document);
+        });
+    });
+
+
+    /**
+     * This button is in the "action buttons" group. They act upon the list of file download options. This
+     * fires all the checkboxes' click events, causing them all the download.
+     */
+    document.getElementById('getAllFileOptsButton').addEventListener('click', function onDigFileOptions() {
+        document.querySelectorAll('input[type="checkbox"]').forEach(function makeDescriptors(cbEl) {
+           var evt = new MouseEvent('click');
+           cbEl.dispatchEvent(evt);
+        });
+    });
+
+
+    /**
+     * 
+     */
+    document.getElementById('scrapeFileOptionsButton').addEventListener('click', function onDigFileOptions() {
+        chrome.runtime.getBackgroundPage(function doScrapingForOptions(bgWindow) {
+            bgWindow.goScrapeFileOptions(window.document);
+        });
+    });
+
     /**
      * Scrape a page, picking up all the included images.
      */
