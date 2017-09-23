0b59b9ed566214cfcf1e276bbb253f0f8f2c79d1
Sid Spencer <dis@bub.local>
2017-09-22 19:12:02 -0500

    New singleton service for looking at webRequests

---
+++ background/Voyeur.js
@@ -0,0 +1,41 @@
+'use strict'
+
+/** 
+ * Singleton.
+ * Web Navigation listener for GimmeGimmieGimmie. Listens to web requests in order to 
+ * see what media is being requested.
+ */
+var Voyeur = (function Voyeur(Utils) {
+    // instance object
+    var me = {
+        uris: [],
+        isWatching: false,
+    };
+
+    var u = Utils;
+
+    me.start = function start() {
+        if (me.isWatching) { return; };
+
+        u.queryActiveTab()
+        .then(function setUpMediaHeadersListener(tab) {
+            u.addMediaHeadersListener(watchMedia, tab.windowId, tab.id);
+            me.isWatching = true;    
+        });
+    }   
+    
+    function watchMedia(details) {
+        console.log('[Voyeur] pushing uri: [' + details.url + '] type: [' + details.type +']');
+    };
+
+
+    me.stop = function stop() {
+        if (!me.isWatching) { return; };
+        
+        u.removeMediaHeadersListener(watchMedia);
+        me.isWatching = false;
+    };
+
+    // Return our singleton.
+    return me;
+})(Utils);
