ed5ca325aeb62fcd4e2ffe2c4f13b7ddee794de2
sidspencer <sidspencer@gmail.com>
2020-04-23 15:56:45 -0500

    Enable babel and webpack for building with advanced JS.

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+    mode: 'development',
+    context: 'C:\\Users\\wrait\\Source\\sidspencer\\gimme',
+    entry: {
+        background: './background/EventPage.js',
+        content: './content/ContentPeeper.js',
+        options: './options/options.js',
+        popup: './popup/popup.js'
+    },
+    output: {
+        path: 'C:\\Users\\wrait\\Source\\sidspencer\\gimme',
+        filename: './[name]/bundle.js',
+        publicPath: "./",
+        libraryTarget: 'umd',
+        globalObject: 'window',
+        umdNamedDefine: true
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ["@babel/preset-env", {
+                            "useBuiltIns": false,
+                          }],
+                        ],
+                        plugins: [
+                            '@babel/plugin-transform-classes',
+                            ["@babel/plugin-transform-runtime", {
+                                "regenerator": true,
+                            }],
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        modules: [ "node_modules", "./" ]
+    },
+    devtool: "source-map",
+    target: "web"
+};
