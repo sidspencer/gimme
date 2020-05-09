96c2510e3f718261e422b9a6768b1d65479d236a
sidspencer <sidspencer@gmail.com>
2020-05-09 10:33:08 -0500

    Updated configuration files for building the extension.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     mode: 'development',
-    context: 'C:\\Users\\wrait\\Source\\sidspencer\\gimme',
+    // WINDOWS: context: 'C:\\Users\\wrait\\Source\\sidspencer\\gimme',
+    context: '/Users/dis/Source/sidspencer/gimme',
     entry: {
         background: './background/EventPage.js',
         content: './content/ContentPeeper.js',
@@ -8,7 +9,8 @@ module.exports = {
         popup: './popup/popup.js'
     },
     output: {
-        path: 'C:\\Users\\wrait\\Source\\sidspencer\\gimme',
+        // WINDOWS: path: 'C:\\Users\\wrait\\Source\\sidspencer\\gimme',
+        path: '/Users/dis/Source/sidspencer/gimme',
         filename: './[name]/bundle.js',
         publicPath: "./",
         libraryTarget: 'umd',
