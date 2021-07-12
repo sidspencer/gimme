da645c4b9e187ff997efc2d5e1c6de7e951fb80b
thirdlogan <thirdlogan@gmail.com>
2021-07-12 00:59:33 -0500

    Update module pathing.

---
+++ webpack.config.cjs
@@ -0,0 +1,67 @@
+const webpack = require("webpack");
+
+module.exports = {
+    mode: 'development',
+    // WINDOWS: context: 'C:\\Users\\wrait\\Source\\thirdlogan\\gimme',
+    context: '/Users/dis/Source/thirdlogan/gimme',
+    entry: {
+        background: './background/EventPage.js',
+        content: './content/ContentPeeper.js',
+        options: './options/Optionator.js',
+        popup: './popup/Popup.js',
+        baselibs: './baselibs/DataClasses.js'
+    },
+    output: {
+        // WINDOWS: path: 'C:\\Users\\wrait\\Source\\thirdlogan\\gimme',
+        path: '/Users/dis/Source/thirdlogan/gimme',
+        filename: './[name]/bundle.js',
+        publicPath: "./",
+        libraryTarget: 'umd',
+        globalObject: 'window',
+        umdNamedDefine: true
+    },
+    module: {
+        rules:[
+            {
+                test: /\.m?js$/,
+                enforce: 'pre',
+                use: ['source-map-loader']
+            },
+            {
+                test: /\.m?js$/,
+                resolve: {
+                    fullySpecified: false
+                }
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                            'useBuiltIns': 'entry',
+                            'corejs': 3
+                          }],
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties',
+                            '@babel/plugin-transform-classes',
+                            ['@babel/plugin-transform-runtime', {
+                                'regenerator': false,
+                                'useESModules': true,
+                            }],
+                            'es6-promise'
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        modules: [ 'node_modules', './' ]
+    },
+    devtool: "source-map",
+    target: 'web'
+};
